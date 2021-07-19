e31df7e27ccb3c14a1ee9671609d0e676a5fc92a
Ken Murchison <murch@fastmail.com>
2021-07-19 13:50:18 -0400

    httpd.c: treat WS over HTTP/2 tunnel as chunked response (so stream isn't closed)

--- imap/httpd.c
+++ imap/httpd.c
@@ -4696,6 +4696,7 @@ HIDDEN int meth_connect(struct transaction_t *txn, void *params)
         ret = http_proxy_h2_connect(be, txn);
         if (!ret) {
             txn->be = be;
+            txn->flags.te = TE_CHUNKED;
             ptrarray_append(&httpd_streams, txn);
 
             /* Adjust inactivity timer */
